6028953202cadd49fa3cf448000f887b0c9b8680
Vinayak2k03 <vinayaknagar2810@gmail.com>
2025-03-02 23:43:07 +0530

    fixed frontend url

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -9,7 +9,7 @@ import Stripe from "stripe";
 const placeOrder = async (req, res) => {
 
   //If deploying it online, replace the frontend_url with the app url
-  const frontend_url = "http://localhost:5174";
+  const frontend_url = "http://localhost:5173";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
