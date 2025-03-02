ffa04cf05a3e91733d305a505d831e2cad54eec5
Vinayak2k03 <vinayaknagar2810@gmail.com>
2025-03-02 22:20:13 +0530

    init

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://vinayaknagar:!devil!177@cluster0.rwsjs0i.mongodb.net/CampusEase').then(()=>console.log("DB connected"));
+    await mongoose.connect(process.env.DATABASE_URL).then(()=>console.log("DB connected"));
 }
