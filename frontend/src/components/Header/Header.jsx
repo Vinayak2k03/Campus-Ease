250257c06751d160762e0e8589680e30ba59da32
Vinayak2k03 <vinayaknagar2810@gmail.com>
2025-04-02 21:52:30 +0530

    refactor Header component to use dynamic background image and clean up CSS

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Header.css'
+import { assets } from '../../assets/assets'
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header' style={{backgroundImage: `url(${assets.header_img})`}}>
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes.Whether you're craving the bold spices of Indian cuisine, the comforting familiarity of Italian pasta, or the fresh flavors of sushi, our food delivery service brings the world's finest dishes directly to you. </p>
-        <a     href="#explore-menu">View Menu</a>
+        <a href="#explore-menu">View Menu</a>
       </div>
     </div>
   )
