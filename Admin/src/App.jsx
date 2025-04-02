b131a21d5e47a627fa10436ce69ba7cbf4956785
Vinayak2k03 <vinayaknagar2810@gmail.com>
2025-04-02 15:57:14 +0530

    update API base URL to use the new deployment endpoint

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "https://campusease.vinayaknagar.tech/api";
+  const url = "https://campus-ease-backend.onrender.com";
   return (
     <div>
       <ToastContainer />
--- Admin/src/assets/assets.js
+++ Admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://campus-ease-backend.onrender.com'
+export const url = "https://campus-ease-backend.onrender.com"
